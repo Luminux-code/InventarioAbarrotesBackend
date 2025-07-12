7bcac375ef18d73d9023c1c96b51c06040282664
joseph <jovave25@gmail.com>
2025-07-12 12:07:57 -0500

    Actualizo conexión a base de datos Railway

--- index.js
+++ index.js
@@ -10,6 +10,13 @@ app.get('/', (req, res) => {
   res.send('Servidor funcionando');
 });
 
+app.get('/test-db', (req, res) => {
+  connection.query('SELECT 1 + 1 AS resultado', (err, results) => {
+    if (err) return res.status(500).json({ error: err });
+    res.json(results[0]);
+  });
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en puerto ${PORT}`);
